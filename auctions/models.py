f974f1c88a1c6397a65d9f1c7a3cf91d7204cd71
234quix <noirinduggan@gmail.com>
2023-11-24 17:00:42 +0000

    users can create listings and bid on items

--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,52 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+
+    def __str__(self):
+        return f"{self.username}"
+
+
+# Create your models here.
+class AuctionListing(models.Model):
+    OPTION_CHOICES = (
+        ('fashion', 'Fashion'),
+        ('toys', 'Toys'),
+        ('home', 'Home'),
+        ('electronics', 'Electronics')
+    )
+
+    category = models.CharField(
+        max_length=20,
+        choices=OPTION_CHOICES,
+        default='home'  # You can set a default value if needed
+    )
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="seller")
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    url_to_picture = models.URLField(null=True, blank=True)
+    
+
+    def __str__(self):
+        return f"{self.id}: {self.title} (${self.starting_bid})"
+
+
+class Bid(models.Model):
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bids")
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.id} : {self.user} bid {self.offer} on {self.item}"
+
+
+class Comment(models.Model):
+    item = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"Comment by {self.user} on {self.item}"
--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,30 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
+from django.contrib import messages
 
-from .models import User
+from django import forms
+from .models import User, AuctionListing, Bid
+
+class NewListingForm(forms.ModelForm):
+    class Meta:
+        model = AuctionListing
+        fields = ['title', 'description', 'category', 'url_to_picture','starting_bid']
+
+class BidForm(forms.ModelForm):
+    class Meta:
+        model = Bid
+        fields = ['amount']
+
+# def index(request):
+#     return render(request, "auctions/index.html")
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = AuctionListing.objects.all()
+    return render(request, "auctions/index.html", {"listings": listings})
 
 
 def login_view(request):
@@ -61,3 +77,53 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+# def listing(request, listing_id):
+#     listing = get_object_or_404(AuctionListing, pk=listing_id)
+#     bids = Bid.objects.filter(listing=listing).order_by('-timestamp')
+#     return render(request, 'auctions/listing.html', {'listing': listing, 'bids': bids})
+
+
+def listing(request, listing_id):
+    listing = get_object_or_404(AuctionListing, pk=listing_id)
+    bids = Bid.objects.filter(listing=listing).order_by('-timestamp')
+    bid_form = BidForm()
+
+    if request.method == 'POST':
+        bid_form = BidForm(request.POST)
+        if bid_form.is_valid():
+            new_bid = bid_form.save(commit=False)
+            new_bid.bidder = request.user
+            new_bid.listing = listing
+
+            if bids.exists() and new_bid.amount <= bids.first().amount:
+                messages.error(request, 'Bid amount must be higher than the most recent bid.')
+            elif new_bid.amount < listing.starting_bid:
+                messages.error(request, 'Bid amount must be at least as large as the starting bid.')
+            else:
+                new_bid.save()
+                return redirect('listing', listing_id=listing_id)
+        else:
+            messages.error(request, 'Invalid bid amount. Please check the input.')
+
+    return render(request, 'auctions/listing.html', {'listing': listing, 'bids': bids, 'bid_form': bid_form})
+
+
+
+def addlisting(request):
+    if request.method == "POST":
+        form = NewListingForm(request.POST)
+        if form.is_valid():
+
+            form.instance.seller = request.user
+            form.save()
+            #return HttpResponseRedirect(reverse("auctions:index"))
+            return render(request, "auctions/index.html")
+        else:
+            return render(request, "auctions/addlisting.html", {
+                "form": form
+            })
+    else:
+        return render(request, "auctions/addlisting.html", {
+            "form": NewListingForm()
+        })
